78e0c256ab8634b1425c261e396d0bb65a80ee7b
sjmiller85 <miller.scott.j@gmail.com>
2019-06-14 21:32:06 -0400

    remove process-test.js

--- process-test.js
+++
@@ -1,8 +0,0 @@
-const ps = require('ps-node');
-
-ps.lookup({ command: 'node', arguments: 'ingest.js' }, (err, res) => {
-    console.log(res);
-    res.forEach((process) => {
-        console.log(process);
-    })
-})
