4f50827e734a86a25041170d954ca29afc0f21f5
sjmiller85 <miller.scott.j@gmail.com>
2019-06-22 11:12:06 -0400

    account for deleted users and search for new users via popular API call

--- ingest.js
+++ ingest.js
@@ -15,6 +15,12 @@ db.connect().then(response => {
     // Check if lock file exists, and stop process if exists
     return checkIfProcessAlreadyRunning();
 }).then(() => {
+    // Check for new users from the popular list
+    return utils.getRequest('https://megamanmaker.com/megamaker/level/?sort=popular');
+}).then((res) => {
+    //  Check if users already exist in the creators collection
+    return checkAndInsertNewCreators(res);
+}).then(() => {
     // First get a list of all creators that need updating
     return getOutdatedCreators();
 }).then((outdatedCreators) => {
@@ -54,6 +60,9 @@ db.connect().then(response => {
     // Update creators collection with averages
     return updateCreatorsAverages(creators);
 }).then(() => {
+    // Update creators collection with averages
+    return deleteQueue();
+}).then(() => {
     // Exit
     process.exit();
 }).catch(err => {
@@ -81,7 +90,7 @@ const verifyIconsCollection = () => {
             console.log('Icons collection needs to be reset');
             // Icons collection doesn't match the icons bootstrap, flush and re-populate the collection
             return db.deleteMany(collections.icons, {}).then(response => {
-                resolve(true);
+                return true;
             });
         }
         console.log('Icons collection is correct');
@@ -91,7 +100,7 @@ const verifyIconsCollection = () => {
             // icons collection has been emptied, re-populate it
             return db.insertMany(collections.icons, iconsBootstrap).then(response => {
                 console.log('Icons collection has been successfully updated');
-                resolve(response);
+                return response;
             });
         }
         return;
@@ -100,8 +109,25 @@ const verifyIconsCollection = () => {
 
 const getOutdatedCreators = () => {
     const threshold = new Date(new Date().getTime() - config.threshold);
-    return db.find(collections.creators, { updated: { $lt: threshold }, deleted: { $exists: false } });
+    return db.find(collections.creators, { updated: { $lt: threshold }, deleted: { $exists: false } }, false, true); // last parameter throttles the returns
 }
+
+const checkAndInsertNewCreators = (levels) => {
+    const uniques = levels.filter((level, index, self) => self.map(obj => obj.name).indexOf(level.name) === index);
+    const queries = uniques.map(level => {
+        return db.find(collections.creators, { id: level.user }).then(arr => {
+            if (!arr.length) {
+                console.log('Add new creator: ' + level.username);
+                if (!level.username) {
+                    console.log('ERROR!', level);
+                }
+                return db.insertOne(collections.queue, { creator: level.username });
+            }
+        });
+    });
+
+    return Promise.all(queries);
+};
 
 const addOutdatedCreatorsToQueue = (creators) => {
     if (!creators.length) {  // MongoDB may return an empty array
@@ -119,8 +145,11 @@ const addOutdatedCreatorsToQueue = (creators) => {
 
 const getOutdatedLevels = () => {
     console.log('Getting outdated Levels from levels collection');
-    return db.find(collections.levels, { updated: { $lt: new Date( new Date().getTime() - config.threshold ) } });
-}
+    return db.find(collections.levels, { 
+    	deleted: { $exists: false },
+    	updated: { $lt: new Date( new Date().getTime() - config.threshold ) } 
+    });
+};
 
 const addOutdatedLevelsToQueue = (levels) => {
     if (!levels.length) { // MongoDB may return an empty array
@@ -137,19 +166,19 @@ const addOutdatedLevelsToQueue = (levels) => {
 
 const getCreatorsFromQueue = () => {
     console.log('getting creators from queue');
-    return db.find(collections.queue, { creator: { $exists: true } });
+    return db.distinct(collections.queue, 'creator', { creator: { $exists: true } });
 };
 
 const queryForCreatorInfo = (creators) => {
     console.log('Querying the Mega Man Maker API for creators');
     if (!creators.length) return;
-    let requests = creators.map(val => {
+    let requests = creators.map(creator => {
         // Users may be able to submit either an ID or a name which requires one of two API calls
-        if (typeof val.creator === 'string') {
-            return 'https://megamanmaker.com/megamaker/user/name?name=' + val.creator;
+        if (typeof creator === 'string') {
+            return 'https://megamanmaker.com/megamaker/user/name?name=' + creator;
         }
-        if (typeof val.creator === 'number') {
-            return 'https://megamanmaker.com/megamaker/user/' + val.creator;
+        if (typeof creator === 'number') {
+            return 'https://megamanmaker.com/megamaker/user/' + creator;
         }
     });
 
@@ -169,20 +198,31 @@ const updateAndAddCreatorsToDatabase = (creators) => {
         let levels = [];
         let creatorNames = [];
         let newCreators = [];
+        let deletedCreators = creators.filter(creator => creator.error);
 
         for (let creator of creators) {
-            levels = [ ...levels, ...creator.levels ]; // preserve the levels to add/update later on
-            creatorNames.push({ name: creator.name });
+            if (!creator.error) {
+                levels = [ ...levels, ...creator.levels ]; // preserve the levels to add/update later on
+                creatorNames.push({ name: creator.name });
+            }
         } 
 
-        db.find(collections.creators, { $or: creatorNames }).then(res => {
+        deletedCreators = deletedCreators.map(creator => {
+            console.log(creator);
+            return db.updateOne(collections.creators, { name: creator.id }, { $set: {deleted: true }});
+        });
+
+        return Promise.all(deletedCreators).then(() => {
+            return db.find(collections.creators, { $or: creatorNames });
+        }).then(res => {
             newCreators = creators.filter(val => {
-                let match = true;
+                let match = true; // true === new insert
                 for (let i of res) {
-                    if (i.id === val.id) match = false; 
+                    if (i.id === val.id) match = false; // found matching id's, it's an update 
                 }
                 return match;
-            }); 
+            });
+            console.log('UPDATING')
             return updateExistingCreators(res, creators); // pass in the creators array so we can update their score and level count
         }).then(() => {
             return insertNewCreators(newCreators);
@@ -206,6 +246,7 @@ const updateExistingCreators = (dbResults, creators) => {
                 creator.level_size.push({ date: new Date(), level_size: record.level_size });
             }
         }
+
         console.log('Updating creator: ' + creator.name);
         updates.push(db.updateOne(collections.creators, { id: creator.id }, 
             { $set: { score: creator.score, level_size : creator.level_size, icon: creator.icon, admin: creator.admin, updated: new Date() }}))
@@ -215,7 +256,7 @@ const updateExistingCreators = (dbResults, creators) => {
 };
 
 const insertNewCreators = (creators) => {
-    console.log("Inserting creators");
+    console.log('Inserting creators');
     var inserts = [];
 
     for (creator of creators) {
@@ -303,10 +344,11 @@ const removeLevelsFromQueue = (levels) => {
     if (!levels || !levels.length ) {
         return;
     }
-
+    console.log('removing from queue...');
     const ids = levels.map(val => { 
         return { level: val.id } 
     });
+
     return db.deleteMany(collections.queue, { $or: ids });
 };
 
@@ -314,7 +356,6 @@ const removeCreatorsFromQueue = (creators) => {
     if (!creators || !creators.length) {
         return;
     }
-
     const ids = creators.map(val => { 
         return { creator: val.id } 
     });
@@ -328,15 +369,15 @@ const removeCreatorsFromQueue = (creators) => {
 };
 
 const getRemainingLevelsInQueue = () => {
-    return db.find(collections.queue, { level: { $exists: true } });
+    return db.distinct(collections.queue, 'level', { level: { $exists: true } });
 };
 
 const queryForLevelInfo = (levels) => {
     console.log('Getting level info of remaining levels');
     if (!levels || !levels.length) return;
 
-    let requests = levels.map(val => 'https://megamanmaker.com/megamaker/level/info/' + val.level);
-    console.log(requests);
+    let requests = levels.map(level => 'https://megamanmaker.com/megamaker/level/info/' + level);
+
     return utils.runPromisesInSerial(requests, utils.getRequest);
 };
 
@@ -345,28 +386,26 @@ const insertOrUpdateRemainingLevels = (levels) => {
     if (!levels || !levels.length) return;
 
     let updates = levels.map(level => {
-        if(level.error) {
-            if (level.error && level.error === 'Level not found') {
-                console.log('Level is deleted: ' + level.id);
-                return db.updateOne(collections.levels, { id: level.id }, { $set: { deleted: true, update: new Date() } });
-            } else {
-                console.log('Updating Level: ' + level.id);
-                let score = { date: new Date(), score: level.score };
-                let downloads = { date: new Date(), downloads: level.downloads };
-                return db.updateOne(collections.levels, { id: level.id }, 
-                    { $set: { 
-                        id: level.id,
-                        user: level.user,
-                        name: level.name,
-                        username: level.username,
-                        date: new Date(level.date),
-                        boss: level.boss,
-                        updated: new Date() 
-                    }, $addToSet: { 
-                        score: score, 
-                        downloads : downloads 
-                    }}, { upsert: true }); // Upsert in case entry doesn't already exist
-            }
+        if (level.error) {
+            console.log('Level is deleted: ' + level.id);
+            return db.updateOne(collections.levels, { id: level.id }, { $set: { deleted: true, updated: new Date() } });
+        } else {
+            console.log('Updating Level: ' + level.id);
+            let score = { date: new Date(), score: level.score };
+            let downloads = { date: new Date(), downloads: level.downloads };
+            return db.updateOne(collections.levels, { id: level.id }, 
+                { $set: { 
+                    id: level.id,
+                    user: level.user,
+                    name: level.name,
+                    username: level.username,
+                    date: new Date(level.date),
+                    boss: level.boss,
+                    updated: new Date() 
+                }, $addToSet: { 
+                    score: score, 
+                    downloads : downloads 
+                }}, { upsert: true }); // Upsert in case entry doesn't already exist
         }
     });
 
@@ -401,4 +440,10 @@ const updateCreatorsAverages = (creators) => {
     });
 
     return Promise.all(updates);
+};
+
+const deleteQueue = () => {
+    console.log('Clearing queue');
+
+    return db.deleteMany(collections.queue, {});
 };
